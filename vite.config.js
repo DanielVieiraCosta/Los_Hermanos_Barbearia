d4df3b3bba4ac52858372fda08b9082f6699b8af
DanielVieiraCosta <dvieiradacosta777@gmail.com>
2026-09-10 16:27:05 -0300

    correção para hospedagem no Vercel

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,6 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Los_Hermanos_Barbearia/',
-
   plugins: [
     vue(),
     vueDevTools(),
